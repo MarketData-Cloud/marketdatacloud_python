0f4288266821b3c1ba141feb0a65263fe1596fd6
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2018-01-10 11:18:15 +0100

    Add test for 404

--- tests/test_marketdatacloud_python.py
+++ tests/test_marketdatacloud_python.py
@@ -1,5 +1,7 @@
 from marketdatacloud_python import MDC
+from marketdatacloud_python import InvalidSymbolError
 import vcr
+import pytest
 
 @vcr.use_cassette('tests/vcr_cassettes/mdc-daily.yml', filter_query_parameters=['api_key'])
 def test_mdc_info():
@@ -10,3 +12,10 @@ def test_mdc_info():
 
     assert isinstance(response, dict)
     assert response["currency"] == "USD"
+
+@vcr.use_cassette('tests/vcr_cassettes/mdc-daily-error.yml', filter_query_parameters=['api_key'])
+def test_mdc_error():
+    with pytest.raises(InvalidSymbolError):
+        """Tests an API call to get an error day of market data"""
+        mdc = MDC("US0378331005.NASDA")
+        response = mdc.price()
