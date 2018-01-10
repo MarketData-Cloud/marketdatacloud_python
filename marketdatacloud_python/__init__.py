7f62ff68c0fc5e7219b06f19d5fa53232f4100c1
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2018-01-10 09:07:48 +0100

    Implement basic API wrapper

--- marketdatacloud_python/__init__.py
+++ marketdatacloud_python/__init__.py
@@ -1,1 +1,17 @@
+import os
+import requests
+
+MDC_API_KEY = os.environ.get('MDC_API_KEY', None)
+
+class APIKeyMissingError(Exception):
+    pass
+
+if MDC_API_KEY is None:
+    raise APIKeyMissingError(
+        "All methods require an API key. See "
+        "https://marketdata.cloud/dashboard?tab=token."
+        )
+
+headers = { "Authentication": MDC_API_KEY, "Acccept": "application/vnd.marketdatawizards.v1+json" }
+
 from .mdc import MDC
--- marketdatacloud_python/mdc.py
+++ marketdatacloud_python/mdc.py
@@ -1,6 +1,11 @@
-class MDC(object):
-    def __init__(self, id):
-        self.id = id
+from . import headers
+import requests
 
-    def info(self):
-        return {'id': self.id}
+class MDC(object):
+    def __init__(self, symbol):
+        self.symbol  = symbol
+        self.base_url = "https://marketdata.cloud/api/prices/" + symbol
+
+    def price(self):
+        response = requests.get(self.base_url, headers=headers)
+        return response.json()
