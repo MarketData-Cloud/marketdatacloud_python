15b88727fb2242b4df9b2030e17f671db2337781
Ramon Huidobro <ramon.martin.huidobro@gmail.com>
2018-01-10 09:07:35 +0100

    Add casette, fix tests

--- tests/test_marketdatacloud_python.py
+++ tests/test_marketdatacloud_python.py
@@ -1,10 +1,12 @@
 from marketdatacloud_python import MDC
+import vcr
 
+@vcr.use_cassette('tests/vcr_cassettes/mdc-daily.yml', filter_query_parameters=['api_key'])
 def test_mdc_info():
     """Tests an API call to get a day of market data"""
 
-    mdc = MDC("BC074-099A8-963B5-BD650")
-    response = mdc.price("US0378331005.NASDAQ")
+    mdc = MDC("US0378331005.NASDAQ")
+    response = mdc.price()
 
     assert isinstance(response, dict)
-    assert response == 1396, "The ID should be in the response"
+    assert response["currency"] == "USD"
